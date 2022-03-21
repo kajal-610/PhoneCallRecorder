cf4b7f4c98bc00e8e2665bac773f75f184147ca5
Kajal Narang <narangkajal610@gmail.com>
2022-03-22 00:08:56 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/phonecallrecorder/DatabaseSingleton.java
@@ -0,0 +1,13 @@
+package com.example.phonecallrecorder;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+public class DatabaseSingleton {
+    public static SQLiteDatabase database;
+
+    public static SQLiteDatabase getInstance(Context activity){
+        if(database==null)
+            database = new DatabaseHandler(activity).getWritableDatabase();
+        return database;
+    }
+}
---
+++ app/src/main/java/com/example/phonecallrecorder/GenericFileProvider.java
@@ -0,0 +1,8 @@
+package com.example.phonecallrecorder;
+
+
+import androidx.core.content.FileProvider;
+public class GenericFileProvider extends FileProvider {
+    public GenericFileProvider() {
+    }
+}
